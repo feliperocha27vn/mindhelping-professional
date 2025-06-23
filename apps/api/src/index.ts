eb2eb83e584e8754bbbf7238b8e928bb2ff21bfa
Felipe Rocha <feliperocha.dev27@gmail.com>
2025-06-23 17:02:56 -0300

    setup para o backend

--- apps/api/src/index.ts
+++
@@ -1,6 +0,0 @@
-import { ability } from '@saas/auth'
-
-const userCanInviteSomeoneElse = ability.can('invite', 'User')
-const userCanDeleteSomeoneElse = ability.cannot('delete', 'User')
-
-console.log(userCanInviteSomeoneElse, userCanDeleteSomeoneElse)
---
+++ apps/api/src/server.ts
@@ -0,0 +1,40 @@
+import fastifyCors from '@fastify/cors'
+import fastifySwagger from '@fastify/swagger'
+import fastifySwaggerUi from '@fastify/swagger-ui'
+import {
+  jsonSchemaTransform,
+  serializerCompiler,
+  validatorCompiler,
+} from 'fastify-type-provider-zod'
+import { app } from './app'
+import { env } from './env'
+
+app.register(fastifyCors, {
+  origin: true,
+})
+
+app.setSerializerCompiler(serializerCompiler)
+app.setValidatorCompiler(validatorCompiler)
+
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'MindHelping Professional API',
+      version: '1.0.0',
+    },
+  },
+  transform: jsonSchemaTransform,
+})
+
+app.register(fastifySwaggerUi, {
+  routePrefix: '/docs',
+})
+
+app
+  .listen({
+    port: env.PORT,
+    host: '0.0.0.0',
+  })
+  .then(() => {
+    console.log('HTTP Server running 🦅')
+  })
