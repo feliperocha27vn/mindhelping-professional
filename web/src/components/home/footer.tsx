543a2f1d9203b1e77eda2940988140e4ba699ab6
Felipe Rocha <feliperocha.dev27@gmail.com>
2025-07-08 11:39:40 -0300

    feat: responsividade completa, work with us

--- web/src/components/home/footer.tsx
+++ web/src/components/home/footer.tsx
@@ -1,3 +1,4 @@
+import { Mail, Phone } from "lucide-react"
 import {
     Card,
     CardContent,
@@ -5,7 +6,6 @@ import {
     CardHeader,
     CardTitle,
 } from "@/components/ui/card"
-import { Mail, Phone } from "lucide-react"
 
 export function Footer() {
     return (
@@ -25,13 +25,13 @@ export function Footer() {
             <CardFooter className="flex flex-col items-start space-y-2.5">
                 <p className="font-bold text-lg md:text-3xl">Contate - nos</p>
                 <div className="flex items-center space-x-2 md:space-x-4">
-                    <Mail size={20} className="md:hidden"/>
-                    <Mail size={30} className="hidden md:block"/>
+                    <Mail size={20} className="md:hidden" />
+                    <Mail size={30} className="hidden md:block" />
                     <p className="font-light md:text-xl">suporte.mindhelping@gmail.com</p>
                 </div>
                 <div className="flex items-center space-x-2 md:space-x-4">
-                    <Phone size={20} className="md:hidden"/>
-                    <Phone size={30} className="hidden md:block"/>
+                    <Phone size={20} className="md:hidden" />
+                    <Phone size={30} className="hidden md:block" />
                     <p className="font-light md:text-xl">+55 18 99999-0000</p>
                 </div>
             </CardFooter>
--- web/src/components/home/work-with-us.tsx
+++ web/src/components/home/work-with-us.tsx
@@ -1,39 +1,52 @@
 import Image from "next/image";
-import { Button } from "../ui/button";
 import Link from "next/link";
+import { Button } from "../ui/button";
 
 export function WorkWithUs() {
     return (
-        <div className="space-y-2.5 md:space-y-4 md:flex md:flex-col md:items-center">
-            <div className="w-full">
-                <h2 className="text-2xl md:text-4xl font-medium">
-                    Trablhe conosco
-                </h2>
+        <div className="space-y-2.5 md:space-y-4 md:flex md:flex-col md:items-center xl:flex-row xl:justify-between">
+            <div className="flex flex-col items-center gap-y-2.5 xl:self-start xl:w-5/12 xl:space-y-10 xl:mt-16">
+                <div className="flex flex-col items-center space-y-2.5 xl:space-y-10">
+                    <div className="w-full">
+                        <h2 className="text-2xl md:text-4xl font-medium xl:text-5xl">
+                            Trabalhe conosco
+                        </h2>
+                    </div>
+                    <Image
+                        src="/assets/psicologa-persona.png"
+                        alt="Profissional de psicologia"
+                        width={1920}
+                        height={0}
+                        className="md:w-[36rem] xl:hidden"
+                    />
+                    <div className="md:space-y-1">
+                        <p className="font-light md:text-2xl">
+                            Nosso aplicativo leva a saúde mental para todos os cantos do Brasil,
+                            deve ser acessível a todos. Buscamos profissionais comprometidos
+                            com a missão de tornar o cuidado emocional acessível a todos.
+                        </p>
+                        <p className="font-semibold md:text-2xl">
+                            Junte-se a nós e faça
+                            parte de uma comunidade que transforma vidas através da
+                            saúde mental.
+                        </p>
+                    </div>
+                </div>
+                <div className="w-full flex justify-center">
+                    <Link href="/register">
+                        <Button className="cursor-pointer text-2xl py-6 px-9 rounded-4xl md:text-4xl md:py-8 md:px-16 xl:py-9 xl:px-20">
+                            Junte-se a nós
+                        </Button>
+                    </Link>
+                </div>
             </div>
             <Image
                 src="/assets/psicologa-persona.png"
                 alt="Profissional de psicologia"
                 width={1920}
                 height={0}
-                className="md:w-[36rem]"
+                className="hidden xl:block xl:w-[36rem]"
             />
-            <div className="md:space-y-1">
-                <p className="font-light md:text-2xl">
-                    Nosso aplicativo leva a saúde mental para todos os cantos do Brasil,
-                    deve ser acessível a todos. Buscamos profissionais comprometidos
-                    com a missão de tornar o cuidado emocional acessível a todos.
-                </p>
-                <p className="font-semibold md:text-2xl">
-                    Junte-se a nós e faça
-                    parte de uma comunidade que transforma vidas através da
-                    saúde mental.
-                </p>
-            </div>
-            <Link href="/register">
-                <Button className="rounded-3xl w-xl max-w-xl pt-3 pb-11 text-2xl md:text-3xl bg-[#333333] text-white after:flex-1 hover:bg-[#333333]/90">
-                    Junte-se a nós
-                </Button>
-            </Link>
         </div>
     )
 }
